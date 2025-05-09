b241c2208eaa5c4ba08dedf9464fe6bc790a5e10
faisal_farah <farah.faisal@eclectics.io>
2025-05-09 16:16:48 +0300

    add delte todo and fetch by id api

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::net::SocketAddr;
 use axum::response::IntoResponse;
 use axum::{Json, Router};
 use axum::extract::{Path, State};
-use axum::routing::{get, post};
+use axum::routing::{delete, get, post};
 use dotenvy::dotenv;
 use uuid::Uuid;
 use crate::app_state::AppState;
@@ -10,7 +10,7 @@ use crate::common::universal::UniversalResponse;
 use crate::db::init_db;
 use crate::models::todo::CreateTodo;
 use crate::repos::todo_repo;
-use crate::repos::todo_repo::{create_todo, get_all_todos, get_todo_by_id};
+use crate::repos::todo_repo::{create_todo, delete_todo, get_all_todos, get_todo_by_id};
 
 mod app_state;
 mod db;
@@ -33,6 +33,7 @@ async fn main() {
         .route("/api/v1/todo/add",post(add_todo_handler))
         .route("/api/v1/todo/all",post(get_all_todos_handler))
         .route("/api/v1/todo/{todo_id}",post(fetch_by_id_handle))
+        .route("/api/v1/todo/{todo_id}",delete(delete_todo_id))
         .with_state(state);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
@@ -107,6 +108,29 @@ async fn fetch_by_id_handle(
         Ok(todo) => {
             println!("Success");
             UniversalResponse::success("Fetched Todo",todo)
+        }
+        Err(e) => {
+            println!("Error: {e:?}");
+            UniversalResponse::failure(format!("Error: {e:?}"))
+        }
+    }.into_response()
+
+}
+
+
+async fn delete_todo_id(
+    State(state) : State<AppState>,
+    Path(todo_id) : Path<Uuid>
+) -> impl IntoResponse{
+
+    println!("Delete by ID using a Todo !");
+
+    let delete_by_id = delete_todo(&state.db,todo_id).await;
+
+    match delete_by_id {
+        Ok(todo) => {
+            println!("Success");
+            UniversalResponse::success("Deleted Successfully","Deleted Successfully")
         }
         Err(e) => {
             println!("Error: {e:?}");
